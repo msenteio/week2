e6a0ada95e15bdf49ec7edf1ef829ffe6ffa49ae
msenteio <mls866@nyu.edu>
2016-09-27 13:47:26 -0400

    trying to get animations to work -_-

---
+++ Match3/Assets/Scripts/Clearable.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clearable : MatchManagerScript {
+
+	public AnimationClip explode; 
+	private bool isBeingCleared = false; 
+	public bool IsBeingCleared {
+		get { return isBeingCleared; }
+	}
+
+	protected MatchManagerScript token; 
+
+	void Awake(){
+		token = GetComponent<MatchManagerScript> ();
+
+	}
+	// Use this for initialization
+	void Start () {
+
+		 
+	
+	}
+
+	public void Clear(){
+	
+		isBeingCleared = true; 
+		StartCoroutine (ClearCoroutine()); 
+	
+	}
+
+	private IEnumerator ClearCoroutine() {
+		Animator animator = GetComponent<Animator> (); 
+		if (animator) {
+			animator.Play (explode.name);
+			yield return new WaitForSeconds (explode.length); 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
